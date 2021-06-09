64aeb0c02d943851ee7baadbfc440fc254ddcbec
LittleHaku <ivandelhorno@gmail.com>
2021-06-09 13:45:19 +0200

    Template to quick add commands

--- main.js
+++ main.js
@@ -1,39 +1,40 @@
 const Discord = require('discord.js');
+const fs = require('fs');
+const path = require('path');
+
+const config = require('./config.json');
+
+const dotenv = require('dotenv'); //Dont copy my token
+dotenv.config();
 
 
 const client = new Discord.Client();
 
-const prefix = '!';
-
-const fs = require('fs');
 
 client.commands = new Discord.Collection();
 
-const commandFiles = fs.readdirSync('./commands/').filter(file => file.endsWith('.js'));
-for(const file of commandFiles){
-    const command = require(`./commands/${file}`);
-
-    client.commands.set(command.name, command);
-}
-
-
 client.once('ready', () => {
     console.log('Pomodoku is online!');
+
+    const baseFile = 'command-base.js';
+    const commandBase = require(`./commands/${baseFile}`);
+
+    const readCommands = dir => {
+        const files = fs.readdirSync(path.join(__dirname, dir));
+        for (const file of files) {
+            const stat = fs.lstatSync(path.join(__dirname, dir, file))
+            if (stat.isDirectory()) {
+                readCommands(path.join(dir, file))
+            } else if (file !== baseFile) {
+                const option = require(path.join(__dirname, dir, file))
+                commandBase(client, option)
+            }
+        }
+    }
+
+    readCommands('commands');
 });
 
-client.on('message', message => {
-    if(!message.content.startsWith(prefix) || message.author.bot) return; // If the message doesnt start with the prefix or the bot has written it, ignores
-
-    const args = message.content.slice(prefix.length).split(/ +/);
-    const command = args.shift().toLowerCase();
-
-    if(command === 'help'){
-        client.commands.get('help').execute(message, args);
-    }
-    else{
-        message.channel.send('What did you say?');
-    }
-});
 
 
-client.login('ODQ5NjYzMjc4NzY1MzEwMDMz.YLeclw.N6Nx_PWU3qlT10Yagw4hNL8_4Kg');
+client.login(process.env.TOKEN);
